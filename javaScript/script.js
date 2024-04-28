77ecf9fa8b9608691c5ea7f8b0c4b94731790c12
keldaniel123 <keldanie234@gmail.com>
2024-04-28 11:07:52 -0700

    worked on the responsiveness

--- javaScript/script.js
+++ javaScript/script.js
@@ -27,6 +27,14 @@ var mySwiper = new Swiper (".stories", {
       slidesPerView: 1.2,
       spaceBetween: 40, 
     },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+  1024: {
+    slidesPerView: 3,
+    spaceBetween: 25,
+  },
   1076: {
     slidesPerView: 4,
     spaceBetween: 15,
